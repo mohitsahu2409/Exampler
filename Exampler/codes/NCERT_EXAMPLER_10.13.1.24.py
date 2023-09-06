18edd656b90f894c4c650d8d3e47aea05d87c818
mohitsahu2409 <141238665+mohitsahu2409@users.noreply.github.com>
2023-09-07 00:51:08 +0530

    Add files via upload

--- Exampler/codes/NCERT_EXAMPLER_10.13.1.24.py
+++ Exampler/codes/NCERT_EXAMPLER_10.13.1.24.py
@@ -1,26 +1,19 @@
-import random
+import numpy as np                                
+import matplotlib.pyplot as plt
+from scipy.stats import bernoulli
 
-desired_output = 0.2
+#Number of tickits is 40, 1 number divisible by 5 and 0 denotes number not divisible by 5
+simlen=int(1000)
 
+#Probability of the randomly selected ticket number as multiple of 5  i.e., X=1 is 1/8
+prob = 1/8
 
-def simulate():
-  list = []
-  for i in range(0, 1000):
-    list.append(random.randint(1, 40))
-  div_5 = 0
-  for i in range(0, 1000):
-    if list[i] % 5 == 0:
-      div_5 += 1
-  prob_5 = div_5 / 1000
-  return prob_5
-
-
-while True:
-  prob_5 = simulate()
-  if prob_5 == desired_output:
-    print(f"Probability of number divisible by 5 ={prob_5}")
-
-    break
-  else:
-    continue
-
+#Generating sample date using Bernoulli r.v.
+data_bern = bernoulli.rvs(size=simlen,p=prob)
+#Calculating the number of favourable outcomes
+err_ind = np.nonzero(data_bern == 1)
+#calculating the probability
+err_n = round(np.size(err_ind)/simlen,4)
+print(f'simulated probaility is {err_n}')        
+if err_n == prob:
+    print(f'simulated and theopritcal probabilities same and equal to {prob}')
